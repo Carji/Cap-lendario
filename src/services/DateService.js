c5a217027347ea20842ec34d151907232115009c
Carji <juanpascualete12@gmail.com>
2021-09-06 14:38:19 +0200

    updated DateService IsToday()

--- src/services/DateService.js
+++ src/services/DateService.js
@@ -1,8 +1,15 @@
 export class DateService {
     static getDaysOfMonth(date) {
 
-    } 
+    }
     static addMonth(date, diff) {
 
     }
+
+    static isToday(date, newDate) {
+        return (date.getDate() === newDate.getDate()
+            && date.getMonth() === newDate.getMonth()
+            && date.getFullYear() === newDate.getFullYear());
+    }
+
 }
