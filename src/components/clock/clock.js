1cfa4aff745dc31a4fd63ac376b82fb0a37a9d70
juankyop <66223964+juankyop@users.noreply.github.com>
2021-09-07 12:44:33 +0200

    Clock component

---
+++ src/components/clock/clock.js
@@ -0,0 +1,33 @@
+import { FormatService } from "../../services/formatService.js"
+import pubSub from "../../services/pubSub.js"
+import { CHANNELS } from "../../services/config.js"
+
+class Clock extends HTMLElement {
+
+    constructor() {
+        super();
+        this.date = new Date();
+    }
+
+    _formatDate() {
+        return FormatService.getTime(this.date);
+    }
+
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: "open" })
+        const div = document.createElement("div");
+        const text = document.createTextNode(this._formatDate());
+        div.appendChild(text);
+        shadow.appendChild(div);
+        this._dispose = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
+                this.date = date;
+                text.data = this._formatDate();
+        });
+    }
+
+    disconnectedCallback() {
+        this._dispose && this._dispose();
+    }
+}
+
+window.customElements.define("cap-clock", Clock);
