d388f489e978a9371041e46d6f3500932f38e300
Carji <juanpascualete12@gmail.com>
2021-09-07 08:36:34 +0200

    Completed FormatService.js

--- src/services/FormatService.js
+++ src/services/FormatService.js
@@ -1,17 +1,26 @@
-import { DateService} from './DateService.js'
+import { DateService } from './DateService.js'
 
 export class FormatService {
     static getTime(date) {
-        return new Intl.DateTimeFormat('es-ES', { timeStyle: 'medium'}).format(date);
+        return new Intl.DateTimeFormat('es-ES', { timeStyle: 'medium' }).format(date);
     }
     static getSystemDate(date) {
-        return new Intl.DateTimeFormat('es-ES', { dateStyle: 'full'}).format(date);
+        return new Intl.DateTimeFormat('es-ES', { dateStyle: 'full' }).format(date);
     }
+    // static getMonthDate(date) {
+    //     //  return 'Septiembre de 2021'
+    //     let options = { year: 'numeric', month: 'long' };
+    //     return new Intl.DateTimeFormat('es-ES', options).format(date);
+    // }
+    // static getSelectedDate(date) {
+    //     let optionsSelected = { weekday: 'long', day: 'numeric' };
+    //     return DateService.isToday(date, new Date()) ? ' Today ' : new Intl.DateTimeFormat('es-ES', optionsSelected).format(date);
+
     static getMonthDate(date) {
-      //  return 'Septiembre de 2021'
-        return 
+        //  return 'Septiembre de 2021'
+        return new Intl.DateTimeFormat('es-ES',  {  month: 'long',year: 'numeric' }).format(date);
     }
     static getSelectedDate(date) {
-        return DateService.isToday(date,new Date())?' hoy ' : ' lo que me falta por ver'
+        return DateService.isToday(date, new Date()) ? ' Today ' : new Intl.DateTimeFormat('es-ES',{ weekday: 'long', day: 'numeric' }).format(date);
     }
 }
