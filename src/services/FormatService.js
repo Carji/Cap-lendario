879b024a50a4304ce91363a81db91049fb9c5d38
Carji <juanpascualete12@gmail.com>
2021-09-07 08:42:05 +0200

    cleaned comments in code

--- src/services/FormatService.js
+++ src/services/FormatService.js
@@ -7,17 +7,7 @@ export class FormatService {
     static getSystemDate(date) {
         return new Intl.DateTimeFormat('es-ES', { dateStyle: 'full' }).format(date);
     }
-    // static getMonthDate(date) {
-    //     //  return 'Septiembre de 2021'
-    //     let options = { year: 'numeric', month: 'long' };
-    //     return new Intl.DateTimeFormat('es-ES', options).format(date);
-    // }
-    // static getSelectedDate(date) {
-    //     let optionsSelected = { weekday: 'long', day: 'numeric' };
-    //     return DateService.isToday(date, new Date()) ? ' Today ' : new Intl.DateTimeFormat('es-ES', optionsSelected).format(date);
-
     static getMonthDate(date) {
-        //  return 'Septiembre de 2021'
         return new Intl.DateTimeFormat('es-ES',  {  month: 'long',year: 'numeric' }).format(date);
     }
     static getSelectedDate(date) {
