4eb06e7320cd3f460f0169883a5cb93b13cdac7b
Bob-Lennon <brayansc1119@gmail.com>
2021-09-14 09:33:18 +0200

    Added Mixin's

--- src/components/core/mixin.js
+++ src/components/core/mixin.js
@@ -1,12 +1,30 @@
-const MixinGlobal = Base  =>  class extends Base{
+import { PUB_SUB_INSTANCE } from '../../services/config.js';
 
+const MixinGlobal = Base => class extends Base {
+    dispatchGlobal() {
+        const event = new CustomEvent(PUB_SUB_INSTANCE.GLOBAL, {
+            detail: this,
+            bubbles: true,
+            composed: true,
+            cancelable: true,
+        });
+        this.dispatchEvent(event);
+    }
 };
 
-const MixinInstance = Base  =>  class extends Base{
-    
-};
+const MixinInstance = Base => class extends Base {
+    dispatchInstance() {
+        const event = new CustomEvent(PUB_SUB_INSTANCE.INSTANCE, {
+            detail: this,
+            bubbles: true,
+            composed: true,
+            cancelable: true,
+        });
+        this.dispatchEvent(event);
+    }
+}
 
-const Mixin =(classs,...args)=> args.reduce((a,b)=>b(a),classs);
+const Mixin = (classs, ...args) => args.reduce((a, b) => b(a), classs);
 
 export {
     MixinGlobal,
